91bc7744c2965999b7a57ebc1b53f502161c0907
PhamTien-363296 <phamnguyenthuytien3007@gmail.com>
2025-03-07 11:11:28 +0700

    add

--- db/connectMongoDB.js
+++ db/connectMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(`mongodb+srv://phamnguyenthuytien3007:VbaU4wEYdqA0uPPZ@cluster7.7am4h.mongodb.net/qrstock?retryWrites=true&w=majority&appName=Cluster7`)
         console.log(`MongoDB connected: ${conn.connection.host}`)
     }   catch (error){
         console.error(`Error connection to mongoDB: ${error.message}`)
